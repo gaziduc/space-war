30c29b57bcbfe27bcad218242dfdb06cc73e1c81
David Ghiassi <david.ghiassi@epita.fr>
2020-01-09 17:09:17 +0100

    Improved success/failure screen

--- src/end.c
+++ src/end.c
@@ -15,10 +15,10 @@ static void render_success_texts(struct window *window, Uint32 begin)
     else if (alpha == 0)
         alpha = 1;
 
-    SDL_Color green = { .r = 0, .g = 192, .b = 0, .a = alpha };
+    SDL_Color green = { .r = 0, .g = 255, .b = 0, .a = alpha };
 
     render_text(window, window->fonts->zero4b_30, "SUCCESS", green,
-                POS_CENTERED, window->h / 2 - 350);
+                150, 150);
 
     // Print score
     char s[50] = { 0 };
@@ -27,35 +27,35 @@ static void render_success_texts(struct window *window, Uint32 begin)
     SDL_Color orange = { .r = 255, .g = 128, .b = 0, .a = alpha };
 
     render_text(window, window->fonts->zero4b_30_extra_small, s, orange,
-                POS_CENTERED, window->h / 2 - 140);
+                150, 400);
 
     // Health Bonus
     sprintf(s, "Health Bonus: %d", window->health);
 
     render_text(window, window->fonts->zero4b_30_extra_small, s, orange,
-                POS_CENTERED, window->h / 2 - 90);
+                150, 450);
 
     // Bombs Bonus
     int bombs_bonus = window->num_bombs * 100;
     sprintf(s, "Bombs Bonus: %d", bombs_bonus);
 
     render_text(window, window->fonts->zero4b_30_extra_small, s, orange,
-                POS_CENTERED, window->h / 2 - 40);
+                150, 500);
 
     // Difficulty bonus
     sprintf(s, "Difficulty Bonus: %d", window->bonus);
     render_text(window, window->fonts->zero4b_30_extra_small, s, orange,
-                POS_CENTERED, window->h / 2 + 10);
+                150, 550);
 
     // Total
     sprintf(s, "TOTAL: %d", window->score + window->health + bombs_bonus + window->bonus);
 
     render_text(window, window->fonts->zero4b_30_small, s, orange,
-                POS_CENTERED, window->h / 2 + 90);
+                150, 620);
 
     // Enter to continue
-    render_text(window, window->fonts->zero4b_30_extra_small, "CONTINUE: ENTER",
-                green, POS_CENTERED, window->h / 2 + 250);
+    render_text(window, window->fonts->zero4b_30_small, "-> CONTINUE",
+                green, 150, 810);
 }
 
 
@@ -64,6 +64,7 @@ void success(struct window *window)
 {
     Uint32 begin = SDL_GetTicks();
     int escape = 0;
+    int selected = 1;
 
     load_music(window, "data/success.ogg", 1);
 
@@ -71,6 +72,7 @@ void success(struct window *window)
     {
         update_events(window->in, window);
         escape = handle_play_event(window);
+        handle_select_arrow_event(window, &selected, 1);
 
         SDL_RenderClear(window->renderer);
 
@@ -87,7 +89,7 @@ void success(struct window *window)
 }
 
 
-static void render_failure_texts(struct window *window, Uint32 begin)
+static void render_failure_texts(struct window *window, Uint32 begin, int selected)
 {
     Uint32 alpha = SDL_GetTicks() - begin;
 
@@ -96,10 +98,10 @@ static void render_failure_texts(struct window *window, Uint32 begin)
     else if (alpha == 0)
         alpha = 1;
 
-    SDL_Color red = { .r = 192, .g = 0, .b = 0, .a = alpha };
+    SDL_Color red = { .r = 255, .g = 0, .b = 0, .a = alpha };
 
     render_text(window, window->fonts->zero4b_30, "FAILURE", red,
-                POS_CENTERED, window->h / 2 - 250);
+                150, 150);
 
     char s[50] = { 0 };
     sprintf(s, "SCORE: %d", window->score);
@@ -107,13 +109,23 @@ static void render_failure_texts(struct window *window, Uint32 begin)
     SDL_Color orange = { .r = 255, .g = 128, .b = 0, .a = alpha };
 
     render_text(window, window->fonts->zero4b_30_small, s, orange,
-                POS_CENTERED, window->h / 2);
+                150, 450);
 
-    render_text(window, window->fonts->zero4b_30_extra_small, "RETRY: ENTER", red,
-                POS_CENTERED, window->h / 2 + 160);
 
-    render_text(window, window->fonts->zero4b_30_extra_small, "BACK: ESCAPE", red,
-                POS_CENTERED, window->h / 2 + 220);
+    char *s_list[2] = { "-> RETRY", "-> BACK" };
+    SDL_Color blue = { 0, 255, 255, alpha };
+    SDL_Color green = { 0, 255, 0, alpha };
+
+
+    for (int i = 1; i <= 2; i++)
+    {
+        if (selected == i)
+            render_text(window, window->fonts->zero4b_30_small, s_list[i - 1],
+                        green, 150, 730 + (i - 1) * 100);
+        else
+            render_text(window, window->fonts->zero4b_30_small, s_list[i - 1] + 3,
+                        blue, 150, 730 + (i - 1) * 100);
+    }
 }
 
 
@@ -121,15 +133,17 @@ int failure(struct window *window)
 {
     Uint32 begin = SDL_GetTicks();
     int escape = 0;
-    int retry = 0;
+    int selected = 1;
 
     load_music(window, "data/failure.ogg", 1);
 
-    while (!escape && !retry)
+    while (!escape)
     {
         update_events(window->in, window);
         escape = handle_escape_event(window);
-        retry = handle_play_event(window);
+        if (handle_play_event(window))
+            escape = 1;
+        handle_select_arrow_event(window, &selected, 2);
 
         SDL_RenderClear(window->renderer);
 
@@ -138,11 +152,14 @@ int failure(struct window *window)
         SDL_RenderClear(window->renderer);
 
         render_stars(window);
-        render_failure_texts(window, begin);
+        render_failure_texts(window, begin, selected);
         SDL_RenderPresent(window->renderer);
 
         SDL_framerateDelay(window->fps);
     }
+
+    if (selected == 1)
+        escape = 0;
 
     return escape;
 }
