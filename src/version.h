7126a6056562428d0e2d8b855d9a4fa05c7f2453
David Ghiassi <david.ghiassi@gmail.com>
2020-09-04 18:38:57 +0200

    Added Windows version information

---
+++ src/version.h
@@ -0,0 +1,16 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+    #define VERSION_MAJOR   1
+    #define VERSION_MINOR   5
+    #define VERSION_PATCH   0
+
+    #define VERSION_WIN     VERSION_MAJOR,VERSION_MINOR,VERSION_PATCH,0
+    #define VERSION_NUM     VERSION_MAJOR.VERSION_MINOR.VERSION_PATCH\0
+    #define str(n)          #n
+    #define xstr(n)         str(n)
+    #define VERSION_STR     xstr(VERSION_NUM)
+
+    #define DESCRIPTION_STR xstr(Space War VERSION_NUM)
+
+#endif // VERSION_H
