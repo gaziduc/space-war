62516f7dba97ad48d8cd0f15a0b28abdd364fe6d
David Ghiassi <david.ghiassi@gmail.com>
2021-03-22 02:50:27 +0100

    Finalized new build system

--- dep/win64-msvc/SDL2_image/x64/include/SDL2/SDL_image.h
+++ dep/win64-msvc/SDL2_image/x64/include/SDL2/SDL_image.h
@@ -24,9 +24,9 @@
 #ifndef SDL_IMAGE_H_
 #define SDL_IMAGE_H_
 
-#include "SDL.h"
-#include "SDL_version.h"
-#include "begin_code.h"
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_version.h"
+#include "SDL2/begin_code.h"
 
 /* Set up for C function definitions, even when using C++ */
 #ifdef __cplusplus
@@ -156,6 +156,6 @@ extern DECLSPEC int SDLCALL IMG_SaveJPG_RW(SDL_Surface *surface, SDL_RWops *dst,
 #ifdef __cplusplus
 }
 #endif
-#include "close_code.h"
+#include "SDL2/close_code.h"
 
 #endif /* SDL_IMAGE_H_ */
